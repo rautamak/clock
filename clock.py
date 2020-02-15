ac8fcd32e26cb6c4023cd6f770b21d8dc47742d9
rautamak <13891897+rautamak@users.noreply.github.com>
2020-02-15 14:31:03 +0200

    Hour handle, move clock every minute

--- clock.py
+++ clock.py
@@ -1,3 +1,4 @@
+import time
 import math
 import datetime
 from tkinter import *
@@ -6,8 +7,6 @@ WIDTH = 400
 HEIGHT = 400
 
 class Clock:
-    hour_length = 75
-    minute_length = 100
 
     def __init__(self, canvas, pos_x, pos_y):
         self.x = pos_x
@@ -15,18 +14,41 @@ class Clock:
         self.c = canvas
 
     def drawHour(self, x, y):
-        self.c.create_line(WIDTH/2, HEIGHT/2, WIDTH/2 + x, HEIGHT/2 + y, width=7)
+        h = self.c.create_line(WIDTH/2, HEIGHT/2, WIDTH/2 + x * WIDTH/4, HEIGHT/2 + y * WIDTH/4, width=7)
+        return h
 
     def drawMinute(self, x, y):
-        self.c.create_line(WIDTH/2, HEIGHT/2, WIDTH/2 + x * WIDTH/3, HEIGHT/2 + y * WIDTH/3, width=2)
+        m = self.c.create_line(WIDTH/2, HEIGHT/2, WIDTH/2 + x * WIDTH/3, HEIGHT/2 + y * WIDTH/3, width=2)
+        return m
+
+
+    def clear(self, items):
+        for item in items:
+            self.c.delete(item)
+
+def countHourPosition():
+    hour = int(datetime.datetime.now().strftime('%-I')) + int(datetime.datetime.now().strftime('%M')) / 60
+
+    print(hour)
+    hourAngle = (360/12) * hour - 90
+    pos = [math.cos(math.radians(hourAngle)), math.sin(math.radians(hourAngle))]
+
+    return pos
 
 def countMinutePosition():
     min = int(datetime.datetime.now().strftime('%M'))
 
-    minuteAngle = (360/60) * min -90
+    minuteAngle = (360/60) * min - 90
     pos = [math.cos(math.radians(minuteAngle)), math.sin(math.radians(minuteAngle))]
 
     return pos
+
+def runClock(window, clock, h, m):
+    clock.clear([h, m])
+    h = clock.drawHour(countHourPosition()[0], countHourPosition()[1])
+    m = clock.drawMinute(countMinutePosition()[0], countMinutePosition()[1])
+    print("Clock updated")
+    window.after(1000, runClock, window, clock, h, m)
 
 def main():
     window = Tk()
@@ -36,9 +58,10 @@ def main():
     c.pack()
 
     clock = Clock(c, 50, 50)
-#    clock.drawHour()
-    clock.drawMinute(countMinutePosition()[0], countMinutePosition()[1])
+    h = clock.drawHour(countHourPosition()[0], countHourPosition()[1])
+    m = clock.drawMinute(countMinutePosition()[0], countMinutePosition()[1])
 
+    runClock(window, clock, h, m);
     window.mainloop()
 
 main()
